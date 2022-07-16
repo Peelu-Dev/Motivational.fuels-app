6400ac654dadcd42cefe8321d396322a3ef86d0a
peelu123 <raghuwanshipalash6@gmail.com>
2022-07-16 14:36:30 +0530

    Added drawer menu

--- app/src/main/java/com/peelu/motivationfuels/MainActivity.kt
+++ app/src/main/java/com/peelu/motivationfuels/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.peelu.motivationfuels
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.peelu.motivationfuels.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnMenu.setOnClickListener{
+            if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+            }else{
+                binding.drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        binding.navigationLayout.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.share -> {
+                    try {
+                        val shareIntent = Intent(Intent.ACTION_SEND)
+                        shareIntent.type = "text/plain"
+                        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name")
+                        var shareMessage = "Install this App to get Motivational quotes daily"
+                        shareMessage =
+                            """
+                            ${shareMessage}https://play.google.com/store/apps/details?id=${BuildConfig.APPLICATION_ID}
+                            
+                            
+                            """.trimIndent()
+                        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+                        startActivity(Intent.createChooser(shareIntent, "choose one"))
+                    } catch (e: Exception) {
+                        //e.toString();
+                    }
+                    true
+                }
+                R.id.rate -> {
+                    try {
+                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.wordel")))
+                    } catch (e: ActivityNotFoundException) {
+                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.wordel")))
+                    }
+                    true
+                }
+                R.id.more -> {
+                    val uri = Uri.parse("market://details?id=com.wordel")
+                    val myAppLinkToMarket = Intent(Intent.ACTION_VIEW, uri)
+                    try {
+                        startActivity(myAppLinkToMarket)
+                    } catch (e: ActivityNotFoundException) {
+                        Toast.makeText(this, " unable to find market app", Toast.LENGTH_LONG).show()
+                    }
+                    true
+                }else -> {
+                false
+            }
+            }
+        }
+
+    }
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
     }
 }
